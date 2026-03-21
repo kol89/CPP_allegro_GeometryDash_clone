65f5918751920c0a341a861a680f7863aedb07e0
kol89 <58883959+kol89@users.noreply.github.com>
2026-03-21 15:21:00 +0200

    Refactor game classes and improve structure

--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -14,6 +14,9 @@ int attempts = 0;
 int frame = 0;
 int stat = 0;
 int lvl = 1;
+const double g = 900;
+std::string mod;
+bool on_ground = false;
 
 struct colour{
     int r; // red channel
@@ -35,189 +38,267 @@ struct colour{
 };
 
 class shape/*dot*/{
-public:
-    double x; // position x
-    double y; // position y
-    double StartX; // initial position x
-    double StartY; // initial position y
-    double vx; // velocity x
-    double vy; // velocity y
-    double ax; // acceleration x
-    double ay; // acceleration y
-    colour c; // color
+    public:
+        double x; // position x
+        double y; // position y
+        double StartX; // initial position x
+        double StartY; // initial position y
+        double vx; // velocity x
+        double vy; // velocity y
+        double ax; // acceleration x
+        double ay; // acceleration y
+        colour c; // color
 };
 
 class rectangle : public shape{
-public:
-    double sx; // size x
-    double sy; // size y
-    rectangle(double _x, double _y, double _vx, double _vy, double _sx, double _sy, double _ax, double _ay, colour _c){
-        x = _x;
-        y = _y;
-        StartX = _x;
-        StartY = _y;
-        vx = _vx;
-        vy = _vy;
-        sx = _sx;
-        sy = _sy;
-        ax = _ax;
-        ay = _ay;
-        c = _c;
-    }
-    rectangle(){
-        x = 0;
-        y = 0;
-        StartX = 0;
-        StartY = 0;
-        vx = 0;
-        vy = 0;
-        sx = 1;
-        sy = 1;
-        ax = 0;
-        ay = 0;
-        c = colour();
-    }
-    void move(){
-        x += vx/60;
-        y += vy/60;
-    }
-    void accelerate(){
-        vx += ax/60;
-        vy += ay/60;
-    }
-    void rend(){
-        al_draw_filled_rectangle(x, y, x + sx, y + sy, al_map_rgb(c.r, c.g, c.b));
-    }
-    void reset(){
-        x = StartX;
-        y = StartY;
-    }
-    bool chkcol(rectangle a){
-        return a.x < x + sx && a.x + a.sx > x && a.y < y + sy && a.y + a.sy > y;
-    }
+    public:
+        double sx; // size x
+        double sy; // size y
+        rectangle(double _x, double _y, double _vx, double _vy, double _sx, double _sy, double _ax, double _ay, colour _c){
+            x = _x;
+            y = _y;
+            StartX = _x;
+            StartY = _y;
+            vx = _vx;
+            vy = _vy;
+            sx = _sx;
+            sy = _sy;
+            ax = _ax;
+            ay = _ay;
+            c = _c;
+        }
+        rectangle(){
+            x = 0;
+            y = 0;
+            StartX = 0;
+            StartY = 0;
+            vx = 0;
+            vy = 0;
+            sx = 1;
+            sy = 1;
+            ax = 0;
+            ay = 0;
+            c = colour();
+        }
+        void move(){
+            x += vx/60;
+            y += vy/60;
+        }
+        void accelerate(){
+            vx += ax/60;
+            vy += ay/60;
+        }
+        void rend(){
+            al_draw_filled_rectangle(x, y, x + sx, y + sy, al_map_rgb(c.r, c.g, c.b));
+        }
+        void reset(){
+            x = StartX;
+            y = StartY;
+        }
+        bool chkcol(rectangle a){
+            return a.x < x + sx && a.x + a.sx > x && a.y < y + sy && a.y + a.sy > y;
+        }
 };
 
 class player: public rectangle{
-public:
-    char mode;
-    bool flip_gr;
-    rectangle collision_box;
-    rectangle hit_box;
-    player(double _x,double _y,char _mode){
-        mode=_mode;
-        flip_gr = false;
-        x= _x;
-        y= _y;
-        StartX = _x;
-        StartY = _y;
-        vx = 0;
-        vy = 0;
-        sx = 50;
-        sy = 50;
-        ax = 0;
-        ay = 0;
-        c = colour(0,0,0,255);
-        hit_box = rectangle(x,y,vx,vy,sx,sy,ax,ay,colour(255,0,0,0));
-        collision_box = rectangle(x+(sx/3),y+(sy/3),vx,vy,sx/3,sy/3,ax,ay,colour(0,0,255,0));
-    }
-    void sync() {
-        hit_box = rectangle(x,y,vx,vy,sx,sy,ax,ay,colour(255,0,0,0));
-        collision_box = rectangle(x+(sx/3),y+(sy/3),vx,vy,sx/3,sy/3,ax,ay,colour(0,0,255,0));
-    }
-    void pmove() {
-        x += vx/60;
-        y += vy/60;
-        sync();
-    }
-    void paccelerate(){
-        vx += ax/60;
-        if (flip_gr){vy -= ay/60;}else{vy += ay/60;}
-        sync();
-    }
-    void chsp(int s){
-        switch(s){
-            case 0:
-                vx = 25;
-            case 1:
-                vx = 200;
-            case 2:
-                vx = 300;
+    public:
+        char mode;
+        bool flip_gr;
+        rectangle collision_box;
+        rectangle hit_box;
+        player(double _x,double _y,char _mode){
+            mode=_mode;
+            flip_gr = false;
+            x= _x;
+            y= _y;
+            StartX = _x;
+            StartY = _y;
+            vx = 0;
+            vy = 0;
+            sx = 50;
+            sy = 50;
+            ax = 0;
+            ay = 0;
+            c = colour(0,0,0,255);
+            hit_box = rectangle(x,y,vx,vy,sx,sy,ax,ay,colour(255,0,0,0));
+            collision_box = rectangle(x+(sx/3),y+(sy/3),vx,vy,sx/3,sy/3,ax,ay,colour(0,0,255,0));
         }
-        sync();
-    }
+        void sync() {
+            hit_box = rectangle(x,y,vx,vy,sx,sy,ax,ay,colour(255,0,0,0));
+            collision_box = rectangle(x+(sx/3),y+(sy/3),vx,vy,sx/3,sy/3,ax,ay,colour(0,0,255,0));
+        }
+        void pmove() {
+            x += vx/60;
+            y += vy/60;
+            sync();
+        }
+        void paccelerate(){
+            vx += ax/60;
+            if (flip_gr){vy -= ay/60;}else{vy += ay/60;}
+            sync();
+        }
+        void chsp(int s){
+            switch(s){
+                case 0:
+                    vx = 25;
+                case 1:
+                    vx = 200;
+                case 2:
+                    vx = 300;
+            }
+            sync();
+        }
 
-};
-
-class button: public rectangle{
-public:
-    char const *text;
-    bool is_pressed(){
-        ALLEGRO_MOUSE_STATE state;
-        al_get_mouse_state(&state);
-        return state.x >= x & state.y >= y & state.x <= x+sx & state.y <= y+sy & al_mouse_button_down(&state, ALLEGRO_MOUSE_BUTTON_LEFT);
-    }
-    button(double _x, double _y, double _sx, double _sy, char const *_text, colour _c){
-        x = _x;
-        y = _y;
-        sx = _sx;
-        sy = _sy;
-        text = _text;
-        vx = 0;
-        vy = 0;
-        ax=0;
-        ay=0;
-        c = _c;
-    }
-    void brend(){
-        rend();
-        al_draw_text(
-            font,
-            al_map_rgb(0, 0, 0),
-                     (x+sx)/2, (y+sy/3),
-                     ALLEGRO_ALIGN_LEFT,
-                     text
-        );
-    }
-};
-
-rectangle ground(0,300,0,0,700,60,0,0,colour(255,165,0,0));
-class block: public rectangle{
-public:
-    block(double _x, double _y){
-        ax = 0;
-        ay = 0;
-        vx = 0;
-        vy = 0;
-        sx = 50;
-        sy = 50;
-        x = _x*50;
-        y = ground.y-_y*50;
-        StartX = x;
-        StartY = y;
-        c = colour(0,0,255,0);
-    }
-};
-
-class spike: public rectangle{
-public:
-    spike(double _x, double _y){
-        ax = 0;
-        ay = 0;
-        vx = 0;
-        vy = 0;
-        sx = 7.14;
-        sy = 20.71;
-        x = _x*50+20;
-        y = ground.y-_y*50+14.29;
-        StartX = x;
-        StartY = y;
-        c = colour(255,0,0,0);
-    }
 };
 player p = player(0,300, 'c');
 
+rectangle ground(0,300,0,0,700,60,0,0,colour(255,165,0,0));
+class block: public rectangle{
+    public:
+        block(double _x, double _y){
+            ax = 0;
+            ay = 0;
+            vx = 0;
+            vy = 0;
+            sx = 50;
+            sy = 50;
+            x = _x*50;
+            y = ground.y-_y*50;
+            StartX = x;
+            StartY = y;
+            c = colour(0,0,255,0);
+        }
+};
 
-std::vector<rectangle> danger1 = {
+class spike: public rectangle{
+    public:
+        spike(double _x, double _y){
+            ax = 0;
+            ay = 0;
+            vx = 0;
+            vy = 0;
+            sx = 7.14;
+            sy = 20.71;
+            x = _x*50+20;
+            y = ground.y-_y*50+14.29;
+            StartX = x;
+            StartY = y;
+            c = colour(255,0,0,0);
+        }
+};
+
+class button: public rectangle{
+    public:
+        char const *text;
+        bool is_pressed(){
+            ALLEGRO_MOUSE_STATE state;
+            al_get_mouse_state(&state);
+            return state.x >= x & state.y >= y & state.x <= x+sx & state.y <= y+sy & al_mouse_button_down(&state, ALLEGRO_MOUSE_BUTTON_LEFT);
+        }
+        button(double _x, double _y, double _sx, double _sy, char const *_text, colour _c){
+            x = _x;
+            y = _y;
+            sx = _sx;
+            sy = _sy;
+            text = _text;
+            vx = 0;
+            vy = 0;
+            ax=0;
+            ay=0;
+            c = _c;
+        }
+        void brend(){
+            rend();
+            al_draw_text(
+                font,
+                al_map_rgb(0, 0, 0),
+                (x+sx/2)-45, (y+sy/2)-20,
+                ALLEGRO_ALIGN_LEFT,
+                text
+            );
+        }
+};
+
+class level{
+    public:
+    std::vector<spike> danger;
+    std::vector<block> colidable;
+    double end;
+    level(std::vector<spike> _danger, std::vector<block> _colidable, double _end){
+        danger = _danger;
+        colidable = _colidable;
+        end = _end;
+    }
+    void render(){
+        if (danger.empty() == 0){
+            for (int i = 0; i<danger.size(); i++){
+                danger[i].rend();
+            }
+        }
+        if (colidable.empty() == 0){
+            for (int i = 0; i<colidable.size(); i++){
+                colidable[i].rend();
+            }
+        }
+    }
+    bool collision(player p){
+        if (danger.empty() == 0){
+            for (int i = 0; i<danger.size(); i++){
+                        if (p.hit_box.chkcol(danger[i])){
+                            return true;
+                        }
+            }
+        }
+
+        if (colidable.empty() == 0){
+            for (int i = 0; i<colidable.size(); i++){
+                if (p.hit_box.chkcol(colidable[i])){
+                    if(p.flip_gr){
+                        if (p.collision_box.y+p.collision_box.sy>colidable[i].y){
+                            p.y = colidable[i].y+p.hit_box.sy;
+                            p.vy = 0;
+                            on_ground = true;
+                        }
+                    }else{
+                        if (p.collision_box.y+p.collision_box.sy<colidable[i].y){
+                            p.y = colidable[i].y-p.hit_box.sy;
+                            p.vy = 0;
+                            on_ground = true;
+                        }
+                    }
+                    if (p.collision_box.chkcol(colidable[i])){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    void move(){
+        if (p.x>=150){
+            p.x = p.x - p.vx/60;
+            if (danger.empty() == 0){
+                for (int i = 0; i<danger.size(); i++){
+                    danger[i].x = danger[i].x - p.vx/60;
+                }
+            }
+            if (colidable.empty() == 0){
+                for (int i = 0; i<colidable.size(); i++){
+                    colidable[i].x = colidable[i].x - p.vx/60;
+                }
+            }
+        }   
+    }
+};
+
+
+
+
+
+std::vector<spike> danger;
+std::vector<block> colidable;
+level level1(
+    danger = {
     spike(10, 1),
 
     spike(18, 1),
@@ -233,8 +314,8 @@ std::vector<rectangle> danger1 = {
 
     spike(75, 1),
     spike(76, 1),
-};
-std::vector<rectangle> colidable1 = {
+    }, 
+    colidable = {
     block (30, 1),
 
     block (34, 1),
@@ -259,49 +340,56 @@ std::vector<rectangle> colidable1 = {
     block (64, 2),
     block (65, 2),
     block (66, 2),
-};
-
-std::vector<rectangle> danger2 = {
+    }, 
+    1000
+);
+level level2(
+    danger = {
     spike(10, 1),
     spike(11, 1),
     spike(12, 1),
-};
-std::vector<rectangle> colidable2 = {
-    rectangle(0,0,0,0,1,1,0,0,colour(0,0,0,0))
-};
+    }, 
+    colidable = {
+
+    }, 
+    1000
+);
 
 void reset_all(){
     p.reset();
-    if (lvl==1){
-        for (int i = 0; i<danger1.size(); i++){danger1[i].reset();}
-        for (int i = 0; i<colidable1.size(); i++){colidable1[i].reset();}
-    }
-
-    if (lvl==2){
-        for (int i = 0; i<danger2.size(); i++){danger2[i].reset();}
-        for (int i = 0; i<colidable2.size(); i++){colidable2[i].reset();}
+    font = nullptr;
+    switch (lvl){
+        case 1:
+            for (int i = 0; i<level1.danger.size(); i++){level1.danger[i].reset();}
+            for (int i = 0; i<level1.colidable.size(); i++){level1.colidable[i].reset();}
+        case 2:
+            for (int i = 0; i<level2.danger.size(); i++){level2.danger[i].reset();}
+            for (int i = 0; i<level2.colidable.size(); i++){level2.colidable[i].reset();}
     }
     attempts++;
 }
 
-button l1 (100,100,100,50, "level 1", colour(255,255,255,0));
-button l2 (300,100,100,50, "level 2", colour(255,255,255,0));
+button l1 (100,120,170,100, "level 1", colour(255,255,255,0));
+button l2 (350,120,170,100, "level 2", colour(255,255,255,0));
 
 
-const double g = 900;
-std::string mod;
-bool on_ground = false;
+
 //~----------------------Main programm------------------------
 class GameEngine:public Engine{
 
     //*---FPS---+
     virtual void physics_process() override {
         if (stat==0){
+            if (attempts!=0){
+                attempts=0;
+            }
             if (l1.is_pressed()){
+                font = nullptr;
                 lvl=1;
                 stat=1;
             }
             if (l2.is_pressed()){
+                font = nullptr;
                 lvl=2;
                 stat=1;
             }
@@ -353,78 +441,16 @@ class GameEngine:public Engine{
                 p.vy = 0;
                 on_ground = true;
             }
-            if(lvl==1) {
-                for (int i = 0; i<danger1.size(); i++){
-                    if (p.hit_box.chkcol(danger1[i])){
-                        reset_all();
-                    }
-                }
-
-                for (int i = 0; i<colidable1.size(); i++){
-                    if (p.hit_box.chkcol(colidable1[i])){
-                        if(p.flip_gr){
-                            if (p.collision_box.y+p.collision_box.sy>colidable1[i].y){
-                                p.y = colidable1[i].y+p.hit_box.sy;
-                                p.vy = 0;
-                                on_ground = true;
-                            }
-                        }else{
-                            if (p.collision_box.y+p.collision_box.sy<colidable1[i].y){
-                                p.y = colidable1[i].y-p.hit_box.sy;
-                                p.vy = 0;
-                                on_ground = true;
-                            }
-                        }
-                        if (p.collision_box.chkcol(colidable1[i])){
-                            reset_all();;
-                        }
-                    }
-                }
-                if (p.x>=150) {
-                    p.x = p.x - p.vx/60;
-                    for (int i = 0; i<danger1.size(); i++){
-                        danger1[i].x = danger1[i].x - p.vx/60;
-                    }
-                    for (int i = 0; i<colidable1.size(); i++){
-                        colidable1[i].x = colidable1[i].x - p.vx/60;
-                    }
+            if (lvl == 1){
+                level1.move();
+                if(level1.collision(p)){
+                    reset_all();
                 }
             }
-            if(lvl==2) {
-                for (int i = 0; i<danger2.size(); i++){
-                    if (p.hit_box.chkcol(danger2[i])){
-                        reset_all();
-                    }
-                }
-
-                for (int i = 0; i<colidable2.size(); i++){
-                    if (p.hit_box.chkcol(colidable2[i])){
-                        if(p.flip_gr){
-                            if (p.collision_box.y+p.collision_box.sy>colidable2[i].y){
-                                p.y = colidable2[i].y+p.hit_box.sy;
-                                p.vy = 0;
-                                on_ground = true;
-                            }
-                        }else{
-                            if (p.collision_box.y+p.collision_box.sy<colidable2[i].y){
-                                p.y = colidable2[i].y-p.hit_box.sy;
-                                p.vy = 0;
-                                on_ground = true;
-                            }
-                        }
-                        if (p.collision_box.chkcol(colidable2[i])){
-                            reset_all();;
-                        }
-                    }
-                }
-                if (p.x>=150) {
-                    p.x = p.x - p.vx/60;
-                    for (int i = 0; i<danger2.size(); i++){
-                        danger2[i].x = danger2[i].x - p.vx/60;
-                    }
-                    for (int i = 0; i<colidable2.size(); i++){
-                        colidable2[i].x = colidable2[i].x - p.vx/60;
-                    }
+            if (lvl == 2){
+                level2.move();
+                if(level2.collision(p)){
+                    reset_all();
                 }
             }
             
@@ -433,8 +459,6 @@ class GameEngine:public Engine{
                     p.vy = -410;
                 }
             }
-
-            // std::cout<<999;
             frame++;
         }
 
@@ -446,10 +470,18 @@ class GameEngine:public Engine{
             if (font == nullptr) {
                 font = al_load_ttf_font(
                     "src/arial.ttf", // font file
-                    16,         // font size
+                    32,         // font size
                     0           // flags (just use zero)
                 );
             }
+            al_clear_to_color(al_map_rgb(0, 0, 0));
+            al_draw_text(
+                font,
+                al_map_rgb(255, 255, 255),
+                150, 50,
+                ALLEGRO_ALIGN_LEFT,
+                "Geografy Jump -alpha-"
+            );
             l1.brend();
             l2.brend();
         }
@@ -498,21 +530,11 @@ class GameEngine:public Engine{
             p.hit_box.rend();
             p.collision_box.rend();
             ground.rend();
-            if (lvl==1){
-                for (int i = 0; i<danger1.size(); i++){
-                    danger1[i].rend();
-                }
-                for (int i = 0; i<colidable1.size(); i++){
-                    colidable1[i].rend();
-                }
+            if (lvl == 1){
+                level1.render();
             }
-            if (lvl==2){
-                for (int i = 0; i<danger2.size(); i++){
-                    danger2[i].rend();
-                }
-                for (int i = 0; i<colidable2.size(); i++){
-                    colidable2[i].rend();
-                }
+            if (lvl == 2){
+                level2.render();
             }
         }
 
@@ -522,7 +544,6 @@ class GameEngine:public Engine{
 
 
 int main() {
-    //Engine::start(fps4, draw4);
     GameEngine game;
     game.start();
     return 0;
